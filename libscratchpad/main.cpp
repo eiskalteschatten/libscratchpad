a3aad8e7e79b2124db0799bd1396444d95162d03
Alex Seifert <seifert@jochen-schweizer.de>
2018-03-26 09:02:44 +0200

    Initial Commit

---
+++ libscratchpad/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  libscratchpad
+//
+//  Created by Alex Seifert on 26.03.18.
+//  Copyright © 2018 Alex Seifert. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
