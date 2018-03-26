94bee0dcb0bcce3f86f36982b64a3209b7d7df7a
Alex Seifert <seifert@jochen-schweizer.de>
2018-03-26 09:21:09 +0200

    Added Helper class

---
+++ libscratchpad/Helper.cpp
@@ -0,0 +1,13 @@
+//
+//  Helper.cpp
+//  libscratchpad
+//
+//  Created by Alex Seifert on 26.03.18.
+//  Copyright © 2018 Alex Seifert. All rights reserved.
+//
+
+#include "Helper.h"
+
+void Helper::copyFolder(std::string pathToCopy, std::string destination) {
+    
+}
---
+++ libscratchpad/Helper.h
@@ -0,0 +1,21 @@
+//
+//  Helper.h
+//  libscratchpad
+//
+//  Created by Alex Seifert on 26.03.18.
+//  Copyright © 2018 Alex Seifert. All rights reserved.
+//
+
+#ifndef Helper_h
+#define Helper_h
+
+#include <stdio.h>
+#include <string>
+
+class Helper {
+public:
+    
+    void copyFolder(std::string pathToCopy, std::string destination);
+};
+
+#endif /* Helper_h */
