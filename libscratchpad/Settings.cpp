bffd2d516766dc0bc3d5b1ae27d0da340dc325bf
Alex Seifert <seifert@jochen-schweizer.de>
2018-03-26 09:18:03 +0200

    Added Settings class

---
+++ libscratchpad/Settings.cpp
@@ -0,0 +1,15 @@
+//
+//  Settings.cpp
+//  libscratchpad
+//
+//  Created by Alex Seifert on 26.03.18.
+//  Copyright © 2018 Alex Seifert. All rights reserved.
+//
+
+#include "Settings.h"
+
+
+
+// Getters and setters
+
+
---
+++ libscratchpad/Settings.h
@@ -0,0 +1,24 @@
+//
+//  Settings.h
+//  libscratchpad
+//
+//  Created by Alex Seifert on 26.03.18.
+//  Copyright © 2018 Alex Seifert. All rights reserved.
+//
+
+#ifndef Settings_h
+#define Settings_h
+
+#include <stdio.h>
+#include <string>
+
+class Settings {
+private:
+    
+    
+public:
+    
+    // Getters and setters
+};
+
+#endif /* Settings_h */
